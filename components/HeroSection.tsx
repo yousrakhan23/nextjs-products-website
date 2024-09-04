be12127b536b4fd9c48672f1e8790c3996edf674
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-09-04 11:31:35 +0500

    #

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,12 +6,12 @@ const HeroSection = () => {
         
             <section className="flex flex-wrap items-center justify-center px-10 py-5  bg-amber-200 space-x-5">
                 <div className="w-full md:w-1/2 lg:w-1/4 p-4">
-                    <img src="/Images/img1.jpg"/>
+                    
                 </div>
 
                 <div className="w-full md:w-1/2 lg:w-1/4 p-4">
                     <Image
-                        src="/images/image2.jpg"
+                        src="./public/Images/img1.jpg"
                         alt="Image 2"
                         width={300}
                         height={300}
